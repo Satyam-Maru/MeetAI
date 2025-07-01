8d1eb6b2f84fe1d9a30f74cef774ceffc0558f62
Satyam-Maru <satyammaru07@gmail.com>
2025-07-02 00:12:57 +0530

    render configured

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello render')
+})
+
 app.post("/get-token", async (req, res) => {
   const { room, username } = req.body;
 
