7d3c5d587b69a12ab5b5d5d06df8d2d5519d880f
Satyam-Maru <satyammaru07@gmail.com>
2025-08-06 22:54:35 +0530

    video file name changes

--- client/src/components/VideoPlaceholder.jsx
+++ client/src/components/VideoPlaceholder.jsx
@@ -15,7 +15,7 @@ const VideoPlaceholder = () => {
       >
         <source src={LandingPageVideoWebm} type="video/webm" />
         <source src={LandingPageVideoMp4} type="video/mp4" />
-        Your browser does not support the video tag.
+        Your browser does not support the video tag
       </video>
   </div>;
 };
