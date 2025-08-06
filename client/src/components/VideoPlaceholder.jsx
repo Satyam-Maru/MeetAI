e5c550b437c90e143341161ce598d54059591640
Satyam-Maru <satyammaru07@gmail.com>
2025-08-06 22:46:12 +0530

    added landing page video

--- client/src/components/VideoPlaceholder.jsx
+++ client/src/components/VideoPlaceholder.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import '../styles/VideoPlaceholder.css';
+import LandingPageVideoMp4 from '../assets/LandingPageVideo.mp4'
+import LandingPageVideoWebm from '../assets/LandingPageVideo.webm'
+import LandingPageVideoPoster from '../assets/LandingPageVideoPoster.png'
 
 const VideoPlaceholder = () => {
-  return <div className="video-placeholder"></div>;
+  return <div className="video-placeholder">
+    <video
+        className="responsive-video"
+        poster={LandingPageVideoPoster}
+        autoPlay
+        muted
+        playsInline
+      >
+        <source src={LandingPageVideoWebm} type="video/webm" />
+        <source src={LandingPageVideoMp4} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+  </div>;
 };
 
 export default VideoPlaceholder; 
