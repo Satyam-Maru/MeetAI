bd07f6832128249da90368110f2ebbaacbb8addf
Satyam-Maru <satyammaru07@gmail.com>
2025-08-12 18:07:42 +0530

    vercel analytics added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import RoomPage from "./pages/RoomPage";
 import './styles/global.css'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App/>
