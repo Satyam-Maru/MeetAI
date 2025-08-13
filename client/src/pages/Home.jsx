e40fce7e64c4c8c6877dacfd2777e805c63544e5
Satyam-Maru <satyammaru07@gmail.com>
2025-08-13 16:11:17 +0530

    user modal switch link

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,6 +37,12 @@ const Home = () => {
     }
   }, [location.state, isLoggedIn]);
 
+  const clearInfo = () => {
+    setEmail("");
+    setPassword("");
+    setAuthError("");
+  };
+
   return (
     <div className="app-container">
       <div className="content">
@@ -49,7 +55,7 @@ const Home = () => {
             <button
               className="auth-button"
               onClick={() => {
-                setAuthMode('signup');
+                setAuthMode("signup");
                 setShowAuthModal(true);
               }}
             >
@@ -58,7 +64,7 @@ const Home = () => {
             <button
               className="auth-button"
               onClick={() => {
-                setAuthMode('signin');
+                setAuthMode("signin");
                 setShowAuthModal(true);
               }}
             >
@@ -72,9 +78,7 @@ const Home = () => {
         isOpen={showAuthModal}
         onRequestClose={() => {
           setShowAuthModal(false);
-          setShowPassword(false);
-          setEmail('');
-          setPassword('');
+          clearInfo();
         }}
         className={{
           base: "auth-modal",
@@ -98,9 +102,7 @@ const Home = () => {
             className="close-button"
             onClick={() => {
               setShowAuthModal(false);
-              setEmail('');
-              setPassword('');
-              setShowPassword(false);
+              clearInfo();
             }}
           >
             <svg
@@ -132,60 +134,50 @@ const Home = () => {
             }}
             required
           />
-          <div className="password-wrapper">
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-                setAuthError(""); // Clear error on change
-              }}
-              required
-            />
-            <button
-              type="button"
-              className="password-toggle-btn"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
-                  <circle cx="12" cy="12" r="3" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" />
-                  <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68" />
-                  <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61" />
-                  <line x1="2" x2="22" y1="2" y2="22" />
-                </svg>
-              )}
-            </button>
-          </div>
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setAuthError(""); // Clear error on change
+            }}
+            required
+          />
           <button onClick={() => handleEmailLogin(authMode === "signup")}>
             Continue
           </button>
           <div className="divider">or</div>
           <AuthButtons onSuccess={handleLoginSuccess} />
+          <div className="auth-toggle">
+            {authMode === "signin" ? (
+              <>
+                Don't have an account?{" "}
+                <button
+                  className="auth-toggle-link"
+                  onClick={() => {
+                    setAuthMode("signup");
+                    clearInfo();
+                  }}
+                >
+                  Sign Up
+                </button>
+              </>
+            ) : (
+              <>
+                Already have an account?{" "}
+                <button
+                  className="auth-toggle-link"
+                  onClick={() => {
+                    setAuthMode("signin");
+                    clearInfo();
+                  }}
+                >
+                  Sign In
+                </button>
+              </>
+            )}
+          </div>
         </div>
       </Modal>
     </div>
