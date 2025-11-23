deec4b41afbb542cf24273daa0afb2058bf33fa6
Satyam-Maru <satyammaru07@gmail.com>
2025-11-23 18:20:22 +0530

    removed vercel cron due to limitation

--- client/api/keepAlive.js
+++
@@ -1,8 +0,0 @@
-export default handler = async (req, res) =>  {
-    try{
-        await fetch(import.meta.env.VITE_BACKEND_URL);
-        res.status(200).json({ok : true});
-    }catch{
-        res.status(500).json({error: "Ping Failed."})
-    }
-}
